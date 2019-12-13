a73a36d6b6e7673acf2ed83a2c0c500f83e80d09
iPear <i.pear@outlook.com>
2019-12-13 18:40:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from docx import Document
 from docx.shared import Pt
-from docx.enum.style import WD_STYLE_TYPE
-from docx.shared import Inches
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from os import path
 from selenium.webdriver import DesiredCapabilities
+import PIL
+from PIL import Image, ImageDraw, ImageFont
 
 driver = webdriver.Chrome()
 driver.get('https://oj.neu.edu.cn')
-input('请登录后按回车键')
+input('请登录后按回车键...')
+
 
 def getCode(statusPath):
     driver.get(statusPath)
@@ -42,44 +43,56 @@ for pid in range(652, 705):
     for herolist in bsObj.findAll("tr", {"class": "front-table-row"}):
         id = herolist.td.attrs['title']
         title = herolist.find("td", {"class": "text-left"}).find("a").get_text()
-        print(title + "   https://oj.neu.edu.cn/status/" + id)
-        p = document.add_paragraph()
-        run = p.add_run('      C语言程序设计实验报告')
-        p.bold = True
+        print("已抓取：" + title + "   https://oj.neu.edu.cn/status/" + id)
+
+        para = document.add_paragraph()
+        run = para.add_run('      C语言程序设计实验报告')
+        para.bold = True
         font = run.font
         font.name = u'宋体'
         font.size = Pt(24)
-        p = document.add_paragraph()
-        run = p.add_run("班级： 计算机1805 姓名： {0}  学号： {1} ".format(name, username1))
+
+        para = document.add_paragraph()
+        run = para.add_run("班级： 计算机1805 姓名： {0}  学号： {1} ".format(name, username1))
         run.bold = True
         font = run.font
         font.name = u'宋体'
         font.size = Pt(16)
-        p = document.add_paragraph()
-        run = p.add_run("实验题目：{0}".format(title))
+
+        para = document.add_paragraph()
+        run = para.add_run("实验题目：{0}".format(title))
         run.bold = True
         font = run.font
         font.name = u'宋体'
         font.size = Pt(16)
-        p = document.add_paragraph('     ')
-        run = p.add_run(("对应源代码:"))
+
+        para = document.add_paragraph('     ')
+        run = para.add_run(("对应源代码:"))
         run.bold = True
         font = run.font
         font.name = u'宋体'
         font.size = Pt(16)
-        p = document.add_paragraph()
-        code=getCode("https://oj.neu.edu.cn/status/" + id)
-        run = p.add_run(("code".format(id)))
-        run.bold = True
+
+        para = document.add_paragraph()
+        code = getCode("https://oj.neu.edu.cn/status/" + id)
+        run = para.add_run(code)
+        # run.bold = True
         font = run.font
-        font.name = u'宋体'
-        font.size = Pt(8)
-        p = document.add_paragraph('   ')
-        run = p.add_run("实验结果:")
+        font.name = u'Consolas'
+        font.size = Pt(10)
+
+        para = document.add_paragraph('   ')
+        run = para.add_run("实验结果:")
         run.bold = True
         font = run.font
         font.name = u'宋体'
         font.size = Pt(16)
-        p = document.add_paragraph('   ')
+
+        para = document.add_paragraph('   ')
+        img = Image.new('jpeg', (1080, 720))
+        # print input & output on img here
+
+        para = document.add_paragraph('   ')
         document.add_page_break()
+
 document.save(path.format(username1))

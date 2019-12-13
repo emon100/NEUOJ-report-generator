7f6619ba159d4c307293e2073580b4dd75d5362e
iPear <i.pear@outlook.com>
2019-12-13 18:04:34 +0800

    upload

--- main.py
+++ main.py
@@ -8,24 +8,40 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Inches
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from os import path
+from selenium.webdriver import DesiredCapabilities
 
-name='王一蒙'
-username='20184366'
-username1=20184366
+driver = webdriver.Chrome()
+driver.get('https://oj.neu.edu.cn')
+input('请登录后按回车键')
 
-path="template/{0}.docx"
+def getCode(statusPath):
+    driver.get(statusPath)
+    # input('Press Enter')
+    code = driver.find_element_by_id("paste")
+    text = code.text
+    return text
+
+
+name = '王一蒙'
+username = '20184366'
+username1 = 20184366
+
+path = "template/{0}.docx"
 document = Document(path.format(1))  # 打开文件.docx
-url='https://oj.neu.edu.cn/status/p/1?username={0}&pid={1}&lang=All&result=Accepted'
-url1='https://oj.neu.edu.cn/status/{0}'
+url = 'https://oj.neu.edu.cn/status/p/1?username={0}&pid={1}&lang=All&result=Accepted'
+url1 = 'https://oj.neu.edu.cn/status/{0}'
 
 for pid in range(652, 705):
-    htmlfile = urlopen(url.format(username,pid))
+    htmlfile = urlopen(url.format(username, pid))
     htmlhandle = htmlfile.read()
-    bsObj = BeautifulSoup(htmlhandle,'html.parser')
-    id=''
-    for herolist in bsObj.findAll("tr",{"class":"front-table-row"}):
-        id=herolist.td.attrs['title']
-        title=herolist.find("td",{"class":"text-left"}).find("a").get_text()
+    bsObj = BeautifulSoup(htmlhandle, 'html.parser')
+    id = ''
+    for herolist in bsObj.findAll("tr", {"class": "front-table-row"}):
+        id = herolist.td.attrs['title']
+        title = herolist.find("td", {"class": "text-left"}).find("a").get_text()
         print(title + "   https://oj.neu.edu.cn/status/" + id)
         p = document.add_paragraph()
         run = p.add_run('      C语言程序设计实验报告')
@@ -52,7 +68,8 @@ for pid in range(652, 705):
         font.name = u'宋体'
         font.size = Pt(16)
         p = document.add_paragraph()
-        run = p.add_run(("https://oj.neu.edu.cn/status/{0}".format(id)))
+        code=getCode("https://oj.neu.edu.cn/status/" + id)
+        run = p.add_run(("code".format(id)))
         run.bold = True
         font = run.font
         font.name = u'宋体'
